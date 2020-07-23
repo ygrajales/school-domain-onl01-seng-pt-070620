e65ef650f8749684e7019dc123d7e0e37cd88530
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-23 06:25:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -16,6 +16,15 @@ class School
       @roster[grade] = [name]
     end 
   end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort 
+    @roster.each do |grade, name|
+      name.sort!
+    end
   end
   
 end
